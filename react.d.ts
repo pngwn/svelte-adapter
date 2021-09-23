63ddc74473e898c9600b1f73a6aa27079ed1b590
pngwn <hello@pngwn.io>
2021-09-23 14:38:47 +0100

    feat: add declaration file for react

---
+++ react.d.ts
@@ -0,0 +1,10 @@
+import { SvelteComponent, SvelteComponentTyped } from "svelte";
+import { FC } from "react";
+
+type ExtractProps<Ctor> = Ctor extends new (arg: any) => SvelteComponentTyped<infer Props> ? Props : Record<string, any>;
+
+export default function <CompCtor extends new (arg: any) => SvelteComponent>(
+	component: CompCtor,
+	styles: Record<string, string>,
+	element: string
+): FC<ExtractProps<CompCtor>>
